0629a65b90a992858ad82919b9d3fe1708094fc9
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2016-07-05 14:02:04 -0700

    Add bulk loop check

--- Poll4FX3/Poll4FX3.cpp
+++ Poll4FX3/Poll4FX3.cpp
@@ -6,34 +6,42 @@
 //
 
 #include "stdafx.h"
+unsigned char TestData[] = "Hello Systems Lab!!!";
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	CCyUSBDevice *USBDevice;
-	int Seconds; 
+	int Seconds, i;
+	LONG BytesWritten = 0;
 
 	printf("\nPoll for FX3 device V0.3\n");
 
-	for (Seconds=30; Seconds>0; Seconds--)
+	for (Seconds = 30; Seconds>0; Seconds--)
 	{
 		USBDevice = new CCyUSBDevice(NULL, CYUSBDRV_GUID, true);
-		if (USBDevice->DeviceCount())
+		for (i = 0; i < USBDevice->DeviceCount(); i++)
 		{
-			printf("\nFound %d device(s)", USBDevice->DeviceCount());
-			break;
+			USBDevice->Open(i);
+			printf("\nDev VID = %d, PID = %d", USBDevice->VendorID, USBDevice->ProductID);
+			// A BulkLoop device will have a VID=0x04B4 and a PID=0x00F0
+			if ((USBDevice->VendorID == 0x04B4) && (USBDevice->ProductID == 0x00F0))
+			{
+				BytesWritten = sizeof(TestData);
+				USBDevice->BulkOutEndPt->XferData(TestData, BytesWritten);
+				printf("\nSent %d bytes to FX3", BytesWritten);
+				break;
+			}
+			USBDevice->Close();
 		}
-		else
-		{
-			delete USBDevice;
-			printf("%d \r", Seconds);
-		}
+		if (BytesWritten) break;
+		delete USBDevice;
+		printf("%d \r", Seconds);
 		Sleep(1000);
 	}
 	if (Seconds == 0) printf("Sorry, no FX3 devices found");
 	printf("\nUse CR to EXIT\n");
 	// The DOS box typically exits so fast that the developer doesn't see any messages
 	// Hold the box open until the user enters a character, any character
-	while (!_kbhit()) { }
+	while (!_kbhit()) {}
 	return 0;
 }
-
