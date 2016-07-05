fb5a2c367c4ed9a9655c23a58f3bc19c02c33e49
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2016-07-05 09:05:44 -0700

    Add files for basic poll application.
    Project builds for x86 arch.  Detects attached FX3 devices.

---
+++ Poll4FX3/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+// TODO: reference additional headers your program requires here
+#include <wtypes.h>
+#include <conio.h>
+#include <Dbt.h>
+
+//#include "..\..\CypressLibrary\inc\CyAPI.h"
+//#include "D:\programs\Cypress\EZ-USB FX3 SDK\1.3\library\cpp\inc\CyAPI.h"
+#include <CyAPI.h>
